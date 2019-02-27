3f4c8bf09de9ef986b130b5af1ecdf0f079f59d5
atmelino <tmbscience@gmail.com>
2019-02-27 02:25:29 -0600

    print INA3221 values as hex

--- Espruino/develop/INA3221/INA3221.js
+++ Espruino/develop/INA3221/INA3221.js
@@ -4,25 +4,44 @@ i2c.setup({ sda: B4, scl: B3 });
 addr = 0x40;
 
 
-function read() {
+function readWord(register) {
     i2c.writeTo(this.addr, 0x06);
+    i2c.writeTo(this.addr, 0x02);
+    var dbytes = i2c.readFrom(this.addr, 2);
+    print(dbytes[0].toString(16));
+    print(dbytes[1].toString(16));
 
-    i2c.writeTo(this.addr, 0x01);
 
-    var d = i2c.readFrom(this.addr, 8);
-
-    return d;
+    var dword = dbytes[1] | dbytes[0] << 8;
+    if (dword > 32767)
+        dword -= 65536;
+    value = dword * 0.005;
+    return value;
 
 }
 
 
 
 function start() {
-    setInterval(function () {
-        console.log('reading..');
-        var data = read();
-        print(data.toString());
-    }, 2000);
+    console.log('configuration register');
+    //var config = read(0);
+    //print(config.toString());
+
+    var i;
+    for (i = 0; i < 5; i++) {
+        var shuntVoltage1 = readWord(0x01);
+        print("shuntVoltage1 " + shuntVoltage1.toString());
+        //var busVoltage1 = readWord(0x02);
+        //print("busVoltage1 " + busVoltage1.toString());
+    }
+
+    // setInterval(function () {
+    //     console.log('reading..');
+    //     var shuntVoltage1 = read(2);
+    //     print("shuntVoltage1 " + shuntVoltage1.toString());
+    //     var busVoltage1 = read(4);
+    //     print("busVoltage1 " + busVoltage1.toString());
+    // }, 2000);
 }
 
 start();
