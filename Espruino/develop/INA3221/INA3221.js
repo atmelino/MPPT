f3a1f8edcf55ef1ddcd5498932f7ac92cb60e113
atmelino <tmbscience@gmail.com>
2019-02-27 18:04:28 -0600

    INA3221

--- Espruino/develop/INA3221/INA3221.js
+++ Espruino/develop/INA3221/INA3221.js
@@ -1,52 +1,63 @@
+var interval;
+
 // Setup I2C
 var i2c = new I2C();
 i2c.setup({ sda: B4, scl: B3 });
 addr = 0x40;
 
-
 function readWord(register) {
     i2c.writeTo(this.addr, 0x06);
-    i2c.writeTo(this.addr, 0x02);
+    i2c.writeTo(this.addr, register);
     var dbytes = i2c.readFrom(this.addr, 2);
-    var line = dbytes[0].toString(16) + dbytes[1].toString(16);
-    print(line);
-    //print(dbytes[0].toString(16));
-    //print(dbytes[1].toString(16));
-    //Serial.write(dbytes[0].toString(16));
-
-
     var dword = dbytes[1] | dbytes[0] << 8;
     if (dword > 32767)
         dword -= 65536;
-    value = dword * 0.005;
-    return value;
 
+    //var line = dbytes[0].toString(16) + dbytes[1].toString(16) + " " + dword.toString(16);
+    //print(line);
+
+    return dword;
+}
+
+function getShuntVoltage(channel) {
+    return readWord(0x01) * 0.005;
+}
+
+function getBusVoltage(channel) {
+    return readWord(0x02) * 0.001;
 }
 
 
-
 function start() {
+
     console.log('configuration register');
     //var config = read(0);
     //print(config.toString());
 
-    var i;
-    for (i = 0; i < 5; i++) {
-        var shuntVoltage1 = readWord(0x01);
-        print("shuntVoltage1 " + shuntVoltage1.toString());
-        //var busVoltage1 = readWord(0x02);
-        //print("busVoltage1 " + busVoltage1.toString());
-    }
-
-    // setInterval(function () {
-    //     console.log('reading..');
-    //     var shuntVoltage1 = read(2);
+    // var i;
+    // for (i = 0; i < 5; i++) {
+    //     var shuntVoltage1 = getShuntVoltage(1);
     //     print("shuntVoltage1 " + shuntVoltage1.toString());
-    //     var busVoltage1 = read(4);
+    //     var busVoltage1 = getBusVoltage(1);
     //     print("busVoltage1 " + busVoltage1.toString());
-    // }, 2000);
+    // }
+
+    interval = setInterval(function () {
+        //console.log('reading..');
+        var shuntVoltage1 = getShuntVoltage(1);
+        print("shuntVoltage1 " + shuntVoltage1.toString());
+        var busVoltage1 = getBusVoltage(1);
+        print("busVoltage1 " + busVoltage1.toString());
+    }, 2000);
+  
 }
 
 start();
+
+    setWatch(function (e) {
+        console.log("Stop timer");
+        clearInterval(interval);
+    }, BTN, { repeat: true, edge: 'rising' });
+
 
 
