1bc920bd0c50ebceb69c1a359177b8b773b174fa
atmelino <tmbscience@gmail.com>
2019-03-02 03:02:10 -0600

    module

--- Espruino/develop/INA3221/test_INA3221.js
+++ Espruino/develop/INA3221/test_INA3221.js
@@ -1,5 +1,3 @@
-
-
 // Setup I2C
 var i2c = new I2C();
 i2c.setup({ sda: B4, scl: B3 });
@@ -9,44 +7,28 @@ i2c.setup({ sda: B4, scl: B3 });
 //var INA3221 = require("https://raw.githubusercontent.com/atmelino/MPPT/master/Espruino/develop/INA3221/INA3221.js");
 var INA3221 = require("https://github.com/atmelino/MPPT/blob/master/Espruino/develop/INA3221/INA3221.js");
 
-
-
-
-
 var ina = new INA3221(i2c, {
     address: 0x40,
     shunt: 0.1 // the shunt resistor's value
 });
 
+var interval;
 
-// You can now simply read the data
-print ("result:");
+function start() {
+    interval = setInterval(function () {
+        //result1=ina.readChannel1();
+        //var line1 = "channel 1: bus " + result1.busVoltage1.toString() + " V shunt " + result1.shuntVoltage1.toString() + " mV current " + result1.current_mA1 + " mA";
+        //print(line1);
+        result2=ina.readChannel2();
+        var line2 = "channel 2: bus " + result2.busVoltage2.toString() + " V shunt " + result2.shuntVoltage2.toString() + " mV current " + result2.current_mA2 + " mA";
+        print(line2);
+    }, 2000);
+}
 
-print(ina.readChannel2());
+start();
 
-
-
-
-
-
-
-// var interval;
-
-// function start() {
-//     interval = setInterval(function () {
-//         readChannel1();
-//         var line1 = "channel 1: bus " + result.busVoltage1.toString() + " V shunt " + result.shuntVoltage1.toString() + " mV current " + result.current_mA1 + " mA";
-//         print(line1);
-//         readChannel2();
-//         var line2 = "channel 2: bus " + result.busVoltage2.toString() + " V shunt " + result.shuntVoltage2.toString() + " mV current " + result.current_mA2 + " mA";
-//         print(line2);
-//     }, 2000);
-// }
-
-// start();
-
-// setWatch(function (e) {
-//     console.log("Stop timer");
-//     clearInterval(interval);
-// }, BTN, { repeat: true, edge: 'rising' });
+setWatch(function (e) {
+    console.log("Stop timer");
+    clearInterval(interval);
+}, BTN, { repeat: true, edge: 'rising' });
 
