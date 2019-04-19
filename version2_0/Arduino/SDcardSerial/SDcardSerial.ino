700d28666a0104caa45aaa7c60fb6188bd18845e
atmelino <tmbscience@gmail.com>
2019-04-18 23:39:55 -0500

    ard

--- version2_0/Arduino/SDcardSerial/SDcardSerial.ino
+++ version2_0/Arduino/SDcardSerial/SDcardSerial.ino
@@ -11,8 +11,7 @@
 
 // include the SD library:
 #include <SPI.h>
-#include <SD.h>
-#include <ArduinoJson.h>
+#include <mySD.h>
 #include <SoftwareSerial.h>
 
 // set up variables using the SD utility library functions:
@@ -30,122 +29,109 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  Serial.println("program started");
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
 
 }
-void(* resetFunc) (void) = 0;//declare reset function at address 0
 
-void loop() { // run over and over
-  //StaticJsonBuffer<100> jsonBuffer;
-  //char json[50];
+void loop() {
+
   String str;
 
   //read message from Espruino
   while (mySerial.available() > 0) {
     str = mySerial.readStringUntil('\n');
 
-    //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    //delay(1000);                       // wait for a second
-    //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
     //Serial.println("Arduino received:");
     Serial.println(str);
-    //    str.toCharArray(json, 50);
-    //    JsonObject& root = jsonBuffer.parseObject(json);
-    //    if (!root.success()) {
-    //      Serial.println("parseObject() failed");
-    //    }
-    //    else
-    //    {
-    //      if (root.containsKey("PWM")) {
-    //        //Serial.println("PWM found");
-    //        int pwmint = root["PWM"];
-    //        Serial.println(pwmint);
-    //      }
-    //    }
+    //mySerial.println(str);
+    char task = str[0];
 
+    switch (task) {
+      case 'i':    //
+        if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+          Serial.println("init card fail");
+          mySerial.println("init card fail");
+        } else {
+          Serial.println("init card ok");
+          mySerial.write("init card ok\n");
+        }
+        // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+        if (!volume.init(card)) {
+          Serial.println("init volume fail");
+          mySerial.println("init volume fail");
+        } else {
+          Serial.println("init volume ok");
+          mySerial.write("init volume ok\n");
+        }
+        break;
+      case 'l':    //
+        Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+        root.openRoot(volume);
 
-    Serial.print("\nInitializing SD card...");
-
-    // we'll use the initialization code from the utility libraries
-    // since we're just testing if the card is working!
-    if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-      Serial.println("initialization failed.");
-      resetFunc();  //call reset
-
-      //while (1);
-    } else {
-      Serial.println("Wiring is correct and a card is present.");
+        // list all files in the card with date and size
+        root.ls(LS_R | LS_DATE | LS_SIZE);
+        break;
     }
 
-    // print the type of card
-    Serial.println();
-    Serial.print("Card type:         ");
-    switch (card.type()) {
-      case SD_CARD_TYPE_SD1:
-        Serial.println("SD1");
-        break;
-      case SD_CARD_TYPE_SD2:
-        Serial.println("SD2");
-        break;
-      case SD_CARD_TYPE_SDHC:
-        Serial.println("SDHC");
-        break;
-      default:
-        Serial.println("Unknown");
-    }
-
-    // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-    if (!volume.init(card)) {
-      Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-      while (1);
-    }
-
-    Serial.print("Clusters:          ");
-    Serial.println(volume.clusterCount());
-    Serial.print("Blocks x Cluster:  ");
-    Serial.println(volume.blocksPerCluster());
-
-    Serial.print("Total Blocks:      ");
-    Serial.println(volume.blocksPerCluster() * volume.clusterCount());
-    Serial.println();
-
-    // print the type and size of the first FAT-type volume
-    uint32_t volumesize;
-    Serial.print("Volume type is:    FAT");
-    Serial.println(volume.fatType(), DEC);
-
-    volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-    volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-    volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-    Serial.print("Volume size (Kb):  ");
-    //    Serial.println(volumesize);
-    Serial.print("Volume size (Mb):  ");
-    volumesize /= 1024;
-    Serial.println(volumesize);
-    Serial.print("Volume size (Gb):  ");
-    Serial.println((float)volumesize / 1024.0);
-
-    Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-    root.openRoot(volume);
-
-    // list all files in the card with date and size
-    //root.ls(LS_R | LS_DATE | LS_SIZE);
-
-
-    mySerial.write("Arduino sending response to Espruino\n");
-    mySerial.print("Clusters:          ");
-    mySerial.println(volume.clusterCount());
-    mySerial.print("Blocks x Cluster:  ");
-    mySerial.println(volume.blocksPerCluster());
-
-    mySerial.print("Total Blocks:      ");
-    mySerial.println(volume.blocksPerCluster() * volume.clusterCount());
-    mySerial.println();
-
+    delay(10);
 
 
   }
 }
+
+
+
+
+
+//    Serial.print("\nInitializing SD card...");
+//    // we'll use the initialization code from the utility libraries
+//    // since we're just testing if the card is working!
+
+//
+//
+//
+//    Serial.print("Clusters:          ");
+//    Serial.println(volume.clusterCount());
+//    Serial.print("Blocks x Cluster:  ");
+//    Serial.println(volume.blocksPerCluster());
+//
+//    Serial.print("Total Blocks:      ");
+//    Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+//    Serial.println();
+//
+//    // print the type and size of the first FAT-type volume
+//    uint32_t volumesize;
+//    Serial.print("Volume type is:    FAT");
+//    Serial.println(volume.fatType(), DEC);
+//
+//    volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+//    volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+//    volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
+//    Serial.print("Volume size (Kb):  ");
+//    //    Serial.println(volumesize);
+//    Serial.print("Volume size (Mb):  ");
+//    volumesize /= 1024;
+//    Serial.println(volumesize);
+//    Serial.print("Volume size (Gb):  ");
+//    Serial.println((float)volumesize / 1024.0);
+//
+
+//
+//
+//    mySerial.write("Arduino sending response to Espruino\n");
+//    mySerial.print("Clusters:          ");
+//    mySerial.println(volume.clusterCount());
+//    mySerial.print("Blocks x Cluster:  ");
+//    mySerial.println(volume.blocksPerCluster());
+//
+//    mySerial.print("Total Blocks:      ");
+//    mySerial.println(volume.blocksPerCluster() * volume.clusterCount());
+//    mySerial.println();
+//digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//delay(1000);                       // wait for a second
+//digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//void(* resetFunc) (void) = 0;//declare reset function at address 0
+//resetFunc();  //call reset
--- version2_0/Espruino/develop/serial/serial01.js
+++ version2_0/Espruino/develop/serial/serial01.js
@@ -5,29 +5,25 @@
 Serial1.setup(9600, { tx: B6, rx: B7 });
 
 
-Serial1.print("Espruino sending request to Arduino\n");
 
-
-// var arduinoMessage = {
-//     PWM: 21
-// };
-// var arduinoMessageJSON = JSON.stringify(arduinoMessage);
-// Serial1.write(arduinoMessageJSON);
-
+Serial1.print("i:\n");
 
 
 var cmd = "";
 Serial1.on('data', function (data) {
     cmd += data;
+    //print(cmd);
 
-    if (data == '\n') {
-        //    print("<Serial> " + data);
+    if (data === '\n') {
         print(cmd);
         cmd = '';
-
     }
-
-
 });
 
 
+//Serial1.print("Espruino sending request to Arduino\n");
+// var arduinoMessage = {
+//     PWM: 21
+// };
+// var arduinoMessageJSON = JSON.stringify(arduinoMessage);
+// Serial1.write(arduinoMessageJSON);
