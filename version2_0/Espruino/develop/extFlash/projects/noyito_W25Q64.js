10fd00208ea07493bb659148add043aaacf952d7
atmelino <tmbscience@gmail.com>
2019-04-22 03:14:08 -0500

    flash memory tests

--- version2_0/Espruino/develop/extFlash/projects/noyito_W25Q64.js
+++ version2_0/Espruino/develop/extFlash/projects/noyito_W25Q64.js
@@ -8,58 +8,88 @@ SPI1.setup({
 });
 
 var myJedec = myflash.getJedec();
+console.log("manufacturer ID: " + myJedec.manufacturerId.toString(16));
+console.log("      device ID: " + myJedec.deviceId.toString(16));
+console.log("capacity: " + myflash.getCapacity());
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
 
 function readPage(page) {
     var x = new Uint8Array(256);
     for (i = 0; i < 256; i++) {
         myflash.seek(page, i);
         x[i] = myflash.read();
+        //myflash.waitReady();
     }
     return x;
 }
 
 
 function hexdump(buffer, blockSize) {
-
     var lines = [];
-    // for (var j = 0; j < buffer.length; j++) {
-    //     console.log("j: " + j + " " + buffer[j].toString(16) + " ");
-    // }
     blockSize = blockSize || 16;
     var myblock = new Uint8Array(blockSize);
     var hex = "0123456789ABCDEF";
     for (var b = 0; b < buffer.length; b += blockSize) {
-
         var addr = ("0000" + b.toString(16)).slice(-4);
-
         var codes = "";
         for (i = 0; i < myblock.length; i++) {
             myblock[i] = buffer[i + b];
             codes += ("0" + myblock[i].toString(16)).slice(-2) + " ";
         }
-
         var chars = "";
         for (i = 0; i < myblock.length; i++) {
             if (myblock[i] < 32 || myblock[i] > 127)
                 myblock[i] = 46;
             chars += String.fromCharCode(myblock[i]);
         }
-
         lines.push(addr + " " + codes + "  " + chars);
     }
-
     return lines.join("\n");
 }
 
-console.log();
-//console.log(hexdump("bla1bla2bla3bla4bla5bla6bla7bla", 16));
+function showPage(number) {
+    console.log("page " + number + ":");
+    console.log(hexdump(readPage(number), 16));
+    // sleep(10000);
+}
+
+function start() {
+
+    console.log();
+
+    for (page = 190; page < 220; page++) {
+        showPage(page);
+    }
+
+    if (false) {
+        console.log("erase 16 pages at 202");
+        myflash.erase16Pages(202);
+    }
+
+    //for (page = 200; page < 220; page++) {
+    //    showPage(page);
+    //}
+
+}
 
 
-console.log("manufacturer ID: " + myJedec.manufacturerId.toString(16));
-console.log("      device ID: " + myJedec.deviceId.toString(16));
-console.log("capacity: " + myflash.getCapacity());
 
-console.log("data before erase");
+
+
+// showPage(199);
+// showPage(200);
+// showPage(201);
+
+
 
 //console.log("page 0:");
 //console.log(hexdump(readPage(0), 16));
@@ -70,20 +100,20 @@ console.log("data before erase");
 
 
 
-console.log("page 1:");
-console.log(hexdump(readPage(1), 16));
-
-console.log("page 199:");
-console.log(hexdump(readPage(199), 16));
-console.log("page 200:");
-console.log(hexdump(readPage(200), 16));
-console.log("page 201:");
-console.log(hexdump(readPage(201), 16));
-
+// console.log("page 1:");
+// console.log(hexdump(readPage(1), 16));
+// console.log("page 199:");
+// console.log(hexdump(readPage(199), 16));
+// console.log("page 200:");
+// console.log(hexdump(readPage(200), 16));
+// console.log("page 201:");
+// console.log(hexdump(readPage(201), 16));
 
 
 
 
+
+//console.log("data before erase");
 // if (true) {
 //     console.log("erase 16 pages at 0");
 //     myflash.erase16Pages(0);
