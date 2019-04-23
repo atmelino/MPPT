798564d2847130e3f7ccb5cea7288c81061d0c0c
atmelino <tmbscience@gmail.com>
2019-04-22 23:03:53 -0500

    server

--- version2_0/Espruino/develop/webserverFlash/projects/webserverFlash.js
+++ version2_0/Espruino/develop/webserverFlash/projects/webserverFlash.js
@@ -7,14 +7,32 @@ var WIFI_OPTIONS = {
     password: ""
 };
 
+myhtml = `
+<html> <body>  <div style="display: table;">  <div style="display: table-row">   Date  <span id="clientDate"></span>  </div>  <div style="display: table-cell; ">  <button>Settings</button>  </div>  </div> </body> </html>
+`;
+
+function onPageRequest(req, res) {
+    var a = url.parse(req.url, true);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<html><body>');
+    // res.write('<p>Pin is ' + (BTN.read() ? 'on' : 'off') + '</p>');
+    // res.write('<a href="?led=1">on</a><br/><a href="?led=0">off</a>');
+    // res.end('</body></html>');
+
+    res.write(myhtml);
+    res.end();
+    if (a.query && "led" in a.query)
+        digitalWrite(LED1, a.query["led"]);
+}
 
 
 // Create and start server
 function startServer() {
-    http.createServer(function (req, res) {
-        res.writeHead(200);
-        res.end("Hello World");
-      }).listen(80);
+    http.createServer(onPageRequest).listen(80);
+    // http.createServer(function (req, res) {
+    //     res.writeHead(200);
+    //     res.end("Hello World");
+    // }).listen(80);
 }
 
 function startWifi() {
