438466d28c1b120dd6c645dc3252ee799a9bd9ac
atmelino <tmbscience@gmail.com>
2019-04-30 02:51:16 -0500

    INA3221

--- version1_1/raspberryPi/script/mount_pi.sh
+++ version1_1/raspberryPi/script/mount_pi.sh
@@ -1,3 +1,5 @@
-sshfs pi@192.168.0.25:/home/pi/github  /home/user/sshfs_pi/
+#sshfs pi@192.168.0.25:/home/pi/github  /home/user/sshfs_pi/
 
-sshfs pi@192.168.2.100:/home/pi/github  /home/user/sshfs_pi/
+#sshfs pi@192.168.1.100:/home/pi/github  /home/user/sshfs_pi/
+
+sshfs pi@192.168.1.5:/home/pi/github  /home/user/sshfs_pi/
